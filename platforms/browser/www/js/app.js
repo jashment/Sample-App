b656b99e61a135d23ab497c2564e95d3247977e5
jashment <jordanashment@gmail.com>
2019-03-17 16:50:25 -0600

    Figuring out what is necessary to commit

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,40 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        window.addEventListener("batterystatus", onBatteryStatus, false);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        //Event Called
+        //console.log('Received Event: ' + id);
+        
+    }
+};
+
+function onBatteryStatus(status) {
+    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+    $('.battery-status').text("Battery Level: " + status.level + "% Plugged in: " + status.isPlugged);
+}
